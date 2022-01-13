5b3815e455859aaf917c10686d55366708c93d16
Nichole McGrew <nicholemcgrew@gmail.com>
2022-01-13 14:23:58 -0600

    commit fizzBuzz

--- app.js
+++ app.js
@@ -3,5 +3,27 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+for (let x = 1; x <= 100; x++) {
+    if (x % 2 == 1){
+       console.log(x);
+    }
+}
+  
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+for (let x = 1; x <= 100; x++){
+
+if (x % 3 == 0 && x % 5 == 0) {
+    console.log("FIZZBUZZ");
+
+} else if (x % 3 == 0) {
+    console.log("FIZZ");
+
+} else if (x % 5 == 0){
+    console.log("BUZZ");
+} else {
+    console.log(x);
+}
+    }
+    
